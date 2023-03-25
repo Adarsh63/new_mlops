5d8247d4f9c901e379da7efdaac30db47f8e98df
Adarsh Tugaon <E007084@cigniti.com>
2023-03-25 18:17:22 +0530

    loging and exception

---
+++ src/exception.py
@@ -0,0 +1,31 @@
+import sys
+import logging
+import loger
+
+# here  we are creating error message exception
+def error_message_details(error, error_detail:sys):
+    # exe_tb will give information related to all error occure
+    _,_,exc_tb=error_detail.exc_info()
+    file_name =exc_tb.tb_frame.f_code.co_filename
+    error_message="Error occurred in python script name [{0}] line number [{1}] error message[{2}]".format()
+    file_name,exc_tb.tb_lineno,str(error)
+
+    return error_message
+
+
+class CustomException(Exception):
+    def __inti__(self, error_message, error_detail:sys):
+        super().__init__(error_message)
+        self.error_message=error_message_details(error_message,error_detail=error_detail)
+
+    def __srt__(self):
+        return self.error_message
+
+"""if __name__ == "__main__":
+    try:
+        a = 1/0
+    except Exception as e:
+        logging.info('Devise by zero')
+        raise CustomException(e,sys)"""
+
+
